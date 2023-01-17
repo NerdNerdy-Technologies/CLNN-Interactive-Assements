e8e6b8eb3ac906710875bde9002683ed0bd90a6b
Piyush-Narsinghani <74352265+Piyush-Narsinghani@users.noreply.github.com>
2023-01-18 02:17:06 +0530

    getParams

--- Nitin/worksheet_1/index.js
+++ Nitin/worksheet_1/index.js
@@ -23,6 +23,22 @@ function loadImages(sources, callback) {
   }
   //console.log("images are",images)
 }
+// 
+function getParams(){
+  var idx = document.URL.indexOf('?');
+  var params = new Array();
+  if (idx != -1) {
+      var pairs = document.URL.substring(idx+1, document.URL.length).split('&');
+      for (var i=0; i<pairs.length; i++){
+          nameVal = pairs[i].split('=');
+          params[nameVal[0]] = nameVal[1];
+      }
+  }
+  return params;
+}
+params = getParams();
+alert(params);
+// 
 
 
 function isNearOutline(animal, outline) {
