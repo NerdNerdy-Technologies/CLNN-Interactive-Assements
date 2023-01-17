e3ac9267d6ef44fb5ab756d6ef4d2fd6ddb25cd2
Piyush-Narsinghani <74352265+Piyush-Narsinghani@users.noreply.github.com>
2023-01-18 04:37:45 +0530

    update

--- Nitin/worksheet_1/index.js
+++ Nitin/worksheet_1/index.js
@@ -2,8 +2,8 @@ import { start, end } from "../../app.js";
 let startTime = start();
 // document.getElementById("param").innerHTML = "New text!";
 
-let params = "hello world";
-document.getElementById("param").innerHTML = params;
+let variable = "hello world";
+document.getElementById("param").innerHTML = variable;
 
 
 var width = window.innerWidth;
