52f6ff555b045e1bb7f2b6ce3a8c6246bb693a39
Piyush-Narsinghani <74352265+Piyush-Narsinghani@users.noreply.github.com>
2023-01-18 09:52:55 +0530

    try

--- Nitin/worksheet_1/index.js
+++ Nitin/worksheet_1/index.js
@@ -33,7 +33,7 @@ function loadImages(sources, callback) {
 function getParams(){
   // var query_string= window.location.search;
   var URLParamsArr = new URLSearchParams(window.location.search)
-  var userID = URLParamsArr.get('userID');
+  var userID =JSON.stringify(URLParamsArr.get('userID'));
   // var idx = document.URL.indexOf('?');
   // var params = new Array();
   // if (idx != -1) {
