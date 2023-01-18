d29e0bf196629d8dfe47b763037c17804f661300
Piyush-Narsinghani <74352265+Piyush-Narsinghani@users.noreply.github.com>
2023-01-18 09:56:44 +0530

    new

--- Nitin/worksheet_1/index.js
+++ Nitin/worksheet_1/index.js
@@ -33,7 +33,7 @@ function loadImages(sources, callback) {
 function getParams(){
   // var query_string= window.location.search;
   var URLParamsArr = new URLSearchParams(window.location.search)
-  var userID =JSON.stringify(URLParamsArr.get('userID'));
+  // var userID =JSON.stringify(URLParamsArr.get('userID'));
   // var idx = document.URL.indexOf('?');
   // var params = new Array();
   // if (idx != -1) {
@@ -44,7 +44,7 @@ function getParams(){
   //     }
   // }
   // return params;
-  return userID;
+  return URLParamsArr;
 }
 var params = getParams();
 
